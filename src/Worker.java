c5d047c2902a8818c11f7143f67907a343edef9b
Шапошникова Дарья <Dora999@mail.ru>
2022-09-18 12:05:31 +0300

    Программа Worker

---
+++ src/Worker.java
@@ -0,0 +1,20 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 30) {
+                errorCallback.onError("Ошибка");
+            } else
+                callback.onDone("Task " + i + " is done");
+        }
+    }
+
+
+}
